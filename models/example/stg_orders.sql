bc8f02160af3fb8906f30aed66ab97b1a16213a5
priyasivaram24@gmail.com <priyasivaram24@gmail.com>
2025-11-12 19:41:50 +0000

    models_created

---
+++ models/example/stg_orders.sql
@@ -0,0 +1,16 @@
+-- Use the `ref` function to select from other models-
+
+SELECT
+    o.order_id,
+    c.customer_id,
+    c.customer_name,
+    p.product_id,
+    p.product_name,
+    o.quantity,
+    o.order_date
+FROM
+    Customers.Orders o
+JOIN
+    {{ ref("stg_customers") }} c ON o.customer_id = c.customer_id
+JOIN
+    Customers.Products p ON o.product_id = p.product_id
